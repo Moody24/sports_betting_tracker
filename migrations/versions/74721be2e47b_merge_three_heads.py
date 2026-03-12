d1ce766fb23d3d24b1caf2fed0c02765815ce2b4
Mohamoud <mohamoudmohamed24@gmail.com>
2026-03-12 00:05:28 -0400

    fix(lint): remove unused imports from merge migration

--- migrations/versions/74721be2e47b_merge_three_heads.py
+++ migrations/versions/74721be2e47b_merge_three_heads.py
@@ -5,9 +5,6 @@ Revises: a1b2c3d4e5f6, a9b8c7d6e5f4, c3d4e5f6a7b8
 Create Date: 2026-03-11 23:49:27.745070
 
 """
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision = '74721be2e47b'
